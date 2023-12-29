0061458c8cd3410bd45af5aa4d1724da3bb56600
Hiep221002 <trandaihiep02@gmail.com>
2023-12-29 13:08:27 +0700

    TranDaiHiep255

---
+++ TranDaiHiep255/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TranDaiHiep255.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
