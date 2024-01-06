d22f0a81a5ce178a7524e94d421a3fa523484e18
Hiep221002 <trandaihiep02@gmail.com>
2024-01-06 13:55:53 +0700

    TDH255

---
+++ TranDaiHiep255/Models/Person.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TranDaiHiep255.Models;
+[Table("Person")]
+public class Person
+{
+    [Key]
+    [Display (Name = "Mã Công dân")]
+    public string PersonID { get; set; }
+    [Display (Name = "Mã căn cước công dân")]
+    public int cccd { get; set;}
+    [Display (Name = "Họ và Tên")]
+
+    public string HoTen { get; set; }
+}
