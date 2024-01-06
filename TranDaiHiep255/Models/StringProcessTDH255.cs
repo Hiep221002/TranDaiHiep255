a028f1c5d1222e2518ffabc82b0df932faed2d40
Hiep221002 <trandaihiep02@gmail.com>
2024-01-06 14:24:04 +0700

    final commit

---
+++ TranDaiHiep255/Models/StringProcessTDH255.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TranDaiHiep255.Models;
+[Table("StringprocessTDH255")]
+public class StringprocessTDH255
+{
+    public string LowerToUpper { get; set; }
+}
